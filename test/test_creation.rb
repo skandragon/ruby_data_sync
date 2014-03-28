fe642934d8558e67fe34629e95dee17343dbf5cb
Michael Graff <explorer@flame.org>
2014-03-27 21:59:21 -0500

    add a test for working config

--- test/test_creation.rb
+++ test/test_creation.rb
@@ -1,9 +1,14 @@
 require 'helper'
 
 class RubyDataSync::TestEngine < Minitest::Test
-  def test_creation_without_arguments
+  def test_creation_without_arguments_raises
     assert_raises (ArgumentError) do
       rds = RubyDataSync::Engine.new
     end
+  end
+
+  def test_with_valid_args_works
+    rds = RubyDataSync::Engine.new(uuid: 'fake-uuid-here')
+    assert rds
   end
 end
